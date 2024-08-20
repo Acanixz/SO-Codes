ef23e110249ab8356412d09f8fac365ee879ae36
Hérick Bittencourt <professional.herickvvb@gmail.com>
2024-08-20 23:08:45 +0000

    Linux process testing

--- Process/proc_linux_ex2.c
+++ Process/proc_linux_ex2.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
+char aux = ' ';
+
 int main()
 {
 	fork();
 
+	aux = getchar();
 	fork();
 
+	aux = getchar();
 	fork();
 	
+	aux = getchar();
 	return 0;
 }
